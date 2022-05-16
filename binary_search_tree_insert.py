b1697030d7d7a9040fb3f282f5fcec1ff869a233
Henrieke Baunack <hbaunack@fb.com>
2022-05-16 07:52:37 -0700

    BST insert function

---
+++ binary_search_tree_insert.py
@@ -0,0 +1,48 @@
+# adding the classes and functions for inserting into a binary search tree
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        inserted = False
+        current_node = self.root
+        while not inserted:
+            if self.root == None:
+                self.root = new_node
+                inserted = True
+            else:
+                if new_node.value <= current_node.value:
+                    if current_node.left:
+                        current_node = current_node.left
+                    else:
+                        current_node.left = new_node
+                        inserted = True
+                elif new_node.value > current_node.value:
+                    if current_node.right:
+                        current_node = current_node.right
+                    else:
+                        current_node.right = new_node
+                        inserted = True
+        return self.root
+
+
+class Node:
+    def __init__(self, val):
+        self.value = val
+        self.left = None
+        self.right = None
+
+    def __repr__(self) -> str:
+        return f'|- Value: {self.value}, Left: {self.left}, Right: {self.right} -|'
+
+
+n1 = Node(8)
+print(n1.left)
+bst1 = BinarySearchTree()
+print(bst1.insert(10))
+print(bst1.insert(3))
+print(bst1.insert(15))
+print(bst1.insert(8))
+print(bst1.insert(2))
+print(bst1.root.value)
--- sandbox.py
+++ sandbox.py
@@ -1,2 +1,27 @@
-list_test = ['hello', 'bye', 'henny']
-print(','.join(l for l in list_test))
+# list_test = ['hello', 'bye', 'henny']
+# print(','.join(l for l in list_test))
+
+test_dict = {
+    'col1': 'alias1',
+    'col2': 'alias2'
+}
+input_table = 'f_HENNY'
+select = 'SELECT '
+counter = 0
+for col in test_dict:
+    print(col)
+    print(test_dict[col])
+    select_line = f'{col} as {test_dict[col]}'
+    print(select_line)
+
+    select += select_line
+    counter += 1
+    if counter == len(test_dict):
+        continue
+    select += ','
+select += f' FROM <INPUT:{input_table}>'
+print(select)
+
+# print(f'Select {list_dict}')
+
+# print(isinstance([4, 5, 4], list))
